2ad725fd9f4b8bda7bc71abf9898eed321b1da57
TheKingMus01 <laarabimustapha5@gmail.com>
2022-09-15 14:43:42 +0100

    initial

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express =require('express') ;
 const path = require('path') ;
 const app = express() ;
-app.use(express.static(_dirname + '/dist/auto-car' )) ;
+app.use(express.static(__dirname + '/dist/auto-car' )) ;
 app.get ( '/*',function ( req , res )
-{ res.sendFile (path.join( _dirname+'/dist/auto-car/index.html'));}) ;
+{ res.sendFile (path.join(__dirname+'/dist/auto-car/index.html'));}) ;
 
 app.listen(process.env.PORT || 8080) ;
